63de264ceba9f236b11f5dc5c1b8c55c80716ecd
harshitgokharu <55587562+harshitgokharu@users.noreply.github.com>
2021-01-02 23:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
-import os
+#import os
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
 from datetime import datetime
 from datetime import timedelta
 import investpy
@@ -10,7 +9,7 @@ import investpy
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-script_path = os.path.dirname(os.path.realpath(__file__))
+#script_path = os.path.dirname(os.path.realpath(__file__))
 end_date = datetime.today().strftime('%d/%m/%Y')
 start_date = datetime.now() - timedelta(days=2*365)
 start_date = start_date.strftime('%d/%m/%Y')
@@ -22,54 +21,26 @@ data_bitcoin = data_bitcoin.iloc[::-1]
 
 
 data_ether = investpy.get_crypto_historical_data(crypto='ethereum', from_date=start_date, to_date=end_date)
-
 data_ether.rename(columns={"Close": "Price"},inplace=True)
 data_ether = data_ether.iloc[::-1]
 
 
 data_xrp = investpy.get_crypto_historical_data(crypto='xrp', from_date=start_date, to_date=end_date)
-
 data_xrp.rename(columns={"Close": "Price"},inplace=True)
 data_xrp = data_xrp.iloc[::-1]
 
 
-data_sp500 = investpy.get_index_historical_data(index='S&P 500',
-                                        country='United States',
-                                        from_date=start_date,
-                                        to_date=end_date)
+data_monero = investpy.get_crypto_historical_data(crypto='monero', from_date=start_date, to_date=end_date)
+data_monero.rename(columns={"Close": "Price"},inplace=True)
+data_monero = data_monero.iloc[::-1]
 
-data_sp500.rename(columns={"Close": "Price"},inplace=True)
-data_sp500 = data_sp500.iloc[::-1]
-
-
-data_comb = pd.DataFrame(index= data_sp500.index)
-list_bitcoin = []
-list_ether = []
-list_xrp = []
-for date1 in data_sp500.index:
-    for date2 in data_bitcoin.index:
-        if date1 == date2:
-            list_bitcoin.append(data_bitcoin.loc[date1,'Price'])
-            continue
-    for date3 in data_ether.index:
-        if date1 == date3:
-            list_ether.append(data_ether.loc[date1,'Price'])
-            continue
-    for date4 in data_xrp.index:
-        if date1 == date4:
-            list_xrp.append(data_xrp.loc[date1,'Price'])
-            continue
             
-data_comb["S&P 500"] = data_sp500['Price']
-data_comb["BTC"] = list_bitcoin  
-data_comb["ETH"] = list_ether  
-data_comb["XRP"] = list_xrp   
+data_comb = pd.DataFrame(index= data_bitcoin.index)
+data_comb["BTC"] = data_bitcoin['Price']
+data_comb["ETH"] = data_ether['Price']  
+data_comb["XRP"] = data_xrp['Price'] 
+data_comb["XMR"] = data_monero['Price'] 
 
-
-data_comb = data_comb.assign(Std_SP500 = lambda x:((x['S&P 500']-x['S&P 500'].min())/(x['S&P 500'].max()-x['S&P 500'].min())* 100))
-data_comb = data_comb.assign(Std_BTC = lambda x:((x['BTC']-x['BTC'].min())/(x['BTC'].max()-x['BTC'].min())* 100))
-data_comb = data_comb.assign(Std_ETH = lambda x:((x['ETH']-x['ETH'].min())/(x['ETH'].max()-x['ETH'].min())* 100))
-data_comb = data_comb.assign(Std_XRP = lambda x:((x['XRP']-x['XRP'].min())/(x['XRP'].max()-x['XRP'].min())* 100))
 
 dates_list = []
 dates_list_label = []
@@ -82,22 +53,74 @@ for i in range(0,len(reverse_data_comb)):
          dates_list.append(reverse_data_comb[i])
          dates_list_label.append(reverse_data_comb[i].strftime('%d/%m/%Y'))
 
+data_comb = data_comb.assign(Perc_BTC = lambda x:((x['BTC']-x['BTC'].mean())/(x["BTC"].mean())* 100))
+data_comb = data_comb.assign(Perc_ETH = lambda x:((x['ETH']-x['ETH'].mean())/(x["ETH"].mean())* 100))
+data_comb = data_comb.assign(Perc_XRP = lambda x:((x['XRP']-x['XRP'].mean())/(x["XRP"].mean())* 100))
+data_comb = data_comb.assign(Perc_XMR = lambda x:((x['XMR']-x['XMR'].mean())/(x["XMR"].mean())* 100))
+
+
 plt.figure(figsize=(100, 40))
-plt.plot(data_comb.index,data_comb['Std_SP500'], label="Standardized S&P 500",linewidth=6.0)
-plt.plot(data_comb.index,data_comb['Std_BTC'], label="Standardized BTC",linewidth=6.0)
-plt.plot(data_comb.index,data_comb['Std_ETH'], label="Standardized ETH",linewidth=6.0)
-plt.plot(data_comb.index,data_comb['Std_XRP'], label="Standardized XRP",linewidth=6.0)
-plt.yticks([0,10,20,30,40,50,60,70,80,90,100], fontsize=50)
+plt.plot(data_comb.index,data_comb['Perc_BTC'], label="Percentage from mean BTC",linewidth=6.0)
+plt.plot(data_comb.index,data_comb['Perc_ETH'], label="Percentage from mean ETH",linewidth=6.0)
+plt.plot(data_comb.index,data_comb['Perc_XRP'], label="Percentage from mean XRP",linewidth=6.0)
+plt.plot(data_comb.index,data_comb['Perc_XMR'], label="Percentage from mean XMR",linewidth=6.0)
+plt.axhline(y=0, color='black', linestyle='-')
+plt.yticks(fontsize=50)
 plt.xticks(dates_list,dates_list_label,fontsize=50,rotation=40)
 plt.legend(loc=0, prop={'size': 60})
     
 plt.savefig('static/images/crypto_std.png')
 
+btc_dec = ""
+eth_dec = ""
+xrp_dec = ""
+xmr_dec = ""
+
+if data_comb["BTC"][0] <= 0.95*data_comb["BTC"].mean():
+    btc_dec= "buy"
+elif data_comb["BTC"][0] > 1.5*data_comb["BTC"].mean():
+    btc_dec= "sell"
+else:
+    btc_dec= "do nothing"
+
+    
+if data_comb["ETH"][0] <= 0.95*data_comb["ETH"].mean():
+    eth_dec= "buy"
+elif data_comb["ETH"][0] > 1.5*data_comb["ETH"].mean():
+    eth_dec= "sell"
+else:
+    eth_dec= "do nothing"
+    
+
+if data_comb["XRP"][0] <= 0.95*data_comb["XRP"].mean():
+    xrp_dec= "buy"
+elif data_comb["XRP"][0] > 1.5*data_comb["XRP"].mean():
+    xrp_dec= "sell"
+else:
+    xrp_dec= "do nothing"
+
+if data_comb["XMR"][0] <= 0.95*data_comb["XMR"].mean():
+    mnr_dec= "buy"
+elif data_comb["XMR"][0] > 1.5*data_comb["XMR"].mean():
+    xmr_dec= "sell"
+else:
+    xmr_dec= "do nothing"
+
+btc_mean= '%.2f'%data_comb["BTC"].mean()
+eth_mean= '%.2f'%data_comb["ETH"].mean()
+xrp_mean= '%.2f'%data_comb["XRP"].mean()
+xmr_mean= '%.2f'%data_comb["XMR"].mean()
+
+btc_today= '%.2f'%data_comb["BTC"][0]
+eth_today= '%.2f'%data_comb["ETH"][0]
+xrp_today= '%.2f'%data_comb["XRP"][0]
+xmr_today= '%.2f'%data_comb["XMR"][0]
+
 
 
 @app.route('/')
 def home():
-    return render_template('index.html',name = 'hello', url='../static/images/crypto_std.png')
+    return render_template('index.html',btc_dec= btc_dec,eth_dec= eth_dec,xrp_dec= xrp_dec,xmr_dec= xmr_dec,btc_mean= btc_mean, eth_mean= eth_mean, xrp_mean= xrp_mean, xmr_mean= xmr_mean, btc_today= btc_today, eth_today= eth_today, xrp_today= xrp_today, xmr_today= xmr_today, url='../static/images/crypto_std.png')
 
 
 @app.route('/disp',methods=['POST'])
