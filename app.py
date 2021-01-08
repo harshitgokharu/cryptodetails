51280a3e0ca35f2de3e6cbd6d60959cc5e37ba21
harshitgokharu <55587562+harshitgokharu@users.noreply.github.com>
2021-01-08 19:43:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,33 @@ plt.yticks([-50,0,50,100,150,200,250,300,350],fontsize=50)
 plt.xticks(dates_list,dates_list_label,fontsize=50,rotation=40)
 plt.legend(loc=0, prop={'size': 60})
     
-#plt.savefig('static/images/crypto_std.png')
+plt.savefig('static/images/crypto_perc.png')
+
+
+plt.figure(figsize=(100, 40))
+plt.plot(data_comb.index,data_comb['BTC'], label="BTC in $",linewidth=6.0)
+plt.yticks(fontsize=50)
+plt.xticks(dates_list,dates_list_label,fontsize=50,rotation=40)
+plt.legend(loc=0, prop={'size': 60})
+    
+plt.savefig('static/images/btc.png')
+
+plt.figure(figsize=(100, 40))
+plt.plot(data_comb.index,data_comb['ETH'], label="ETH in $",linewidth=6.0)
+plt.yticks(fontsize=50)
+plt.xticks(dates_list,dates_list_label,fontsize=50,rotation=40)
+plt.legend(loc=0, prop={'size': 60})
+    
+plt.savefig('static/images/eth.png')
+
+plt.figure(figsize=(100, 40))
+plt.plot(data_comb.index,data_comb['XRP'], label="XRP in $",linewidth=6.0)
+plt.yticks(fontsize=50)
+plt.xticks(dates_list,dates_list_label,fontsize=50,rotation=40)
+plt.legend(loc=0, prop={'size': 60})
+    
+plt.savefig('static/images/xrp.png')
+
 
 
 btc_mean= '%.2f'%data_comb["BTC"].mean()
@@ -76,8 +102,7 @@ xrp_today= '%.2f'%data_comb["XRP"][0]
 
 @app.route('/')
 def home():
-    #return render_template('index.html',btc_mean= btc_mean, eth_mean= eth_mean, xrp_mean= xrp_mean, btc_today= btc_today, eth_today= eth_today, xrp_today= xrp_today, url='../static/images/crypto_std.png')
-    return render_template('index.html',btc_mean= btc_mean, eth_mean= eth_mean, xrp_mean= xrp_mean, btc_today= btc_today, eth_today= eth_today, xrp_today= xrp_today)
+    return render_template('index.html',btc_mean= btc_mean, eth_mean= eth_mean, xrp_mean= xrp_mean, btc_today= btc_today, eth_today= eth_today, xrp_today= xrp_today, url1='../static/images/crypto_perc.png', url2='../static/images/btc.png', url3='../static/images/eth.png', url4='../static/images/xrp.png')
 
 @app.route("/twitter")
 def twitter():
